e080b13a087e9b5535a2e8e2a2ad01350b85c9da
Joice Barbosa <joicevbarbosa@gmail.com>
2021-12-29 14:14:39 -0300

    To-do list

---
+++ script.js
@@ -0,0 +1,35 @@
+/* var checkbox = document. querySelector("#checkbox"); 
+function ativarCheckbox(el) {
+    el. checked = true;
+    ativarCheckbox(checkbox);*/
+
+
+const form = document.getElementById('div');
+const taskList = document.getElementById('tasks');
+
+form.onsubmit = function (e) {
+    e.preventDefault();
+    const inputField = document.createElement('div');
+    addTask(inputField.value);
+    form.requestFullscreen();
+};
+
+function addTask(description) {
+	const taskContainer = document.createElement('div');
+	const newTask = document.createElement('input');
+	const taskLabel = document.createElement('label');
+	const taskDescriptionNode = document.createTextNode(description);
+
+	newTask.setAttribute('type', 'checkbox');
+	newTask.setAttribute('name', description);
+	newTask.setAttribute('id', description);
+
+	taskLabel.setAttribute('for', description);
+	taskLabel.appendChild(taskDescriptionNode);
+
+	taskContainer.classList.add('task-item');
+	taskContainer.appendChild(newTask);
+	taskContainer.appendChild(taskLabel);
+
+	taskList.appendChild(taskContainer);
+}
